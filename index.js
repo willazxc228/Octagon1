aea7996a2540ed6d8a1e7ccf8646cec3779ee586
willazxc228 <polinagrosheva11@gmail.com>
2025-06-06 19:48:53 +1000

    static and dynamic routes

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+
 app.get('/', (req, res) => {
-  res.send('<h1>Привет, Октагон!</h1>');
+  res.send('<h1>Привет, Октагон!</h1>');  
+});
+
+app.get('/static', (req, res) => {
+  res.json({ header: "Hello", body: "Octagon NodeJS Test" });
+});
+
+app.get('/dynamic', (req, res) => {
+  const { a, b, c } = req.query;
+  if (!a || !b || !c || isNaN(a) || isNaN(b) || isNaN(c)) {
+    return res.json({ header: "Error" });
+  }
+  const result = (parseInt(a) * parseInt(b) * parseInt(c)) / 3;
+  res.json({ header: "Calculated", body: result.toString() });
 });
 
 app.listen(PORT, () => {
